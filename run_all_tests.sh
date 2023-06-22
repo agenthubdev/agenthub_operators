87991d08bcc779eefe191338f7292be7edd54559
rbehal <rahulbehal01@hotmail.com>
2023-06-22 13:51:09 -0700

    Added test aws credentials in test script

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -6,6 +6,9 @@ if [ -z "$OPENAI_TOKEN" ]; then
     exit 1
 fi
 
+# Test secrets
+export aws_credentials='{"aws_access_key_id": "test","aws_secret_access_key": "test","aws_region_name": "test"}'
+
 pip install -r ./requirements.txt
 python -m spacy download en_core_web_sm
 
