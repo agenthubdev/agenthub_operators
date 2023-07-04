bd7c228af6d2e39779fa67674266586e1b48c493
MaxBrodeurUrbas <126629752+max-agenthub@users.noreply.github.com>
2023-07-04 16:49:11 -0700

    .py - commit created by https://agenthub.dev/pipeline?run_id=GUJUibme6ykKwQxUqi5Bqa

---
+++ .py
@@ -0,0 +1,23 @@
+```python
+import re
+from .base_operator import BaseOperator
+from ai_context import AiContext
+
+class FilterKeywordOperator(BaseOperator):
+    @staticmethod
+    def declare_name():
+        return 'Filter Keyword Operator'
+
+    @staticmethod
+    def declare_description():
+        return 'This operator filters out items in a list that contain a specified keyword.'
+
+    @staticmethod
+    def declare_category():
+        return BaseOperator.OperatorCategory.MISC.value
+
+    @staticmethod
+    def declare_parameters():
+        return [
+            {
+                "name": "keyword"
