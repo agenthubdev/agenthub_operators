c55db54979cb6c9280ec8f74b4c80dc6ecac01ee
Rahul Behal <137822232+rbehal1729@users.noreply.github.com>
2023-07-11 18:08:22 -0700

    Add file reader/web scraper + Remove ingest data/pdf (#45)
    * ingest data with json + csv
    
    * Add file reader/web scraper+Remove ingest data/pdf
    
    * Fix docs
    
    * Remove print logs
    
    * Added description and changed placholder
    
    ---------
    
    Co-authored-by: MaxBrodeurUrbas <max@garnr.io>

--- docs/__init__.md
+++ docs/__init__.md
@@ -11,6 +11,7 @@ The inputs vary depending on the specific operator. Broadly, the inputs include 
 ## Parameters
 
 Depending on the operator, the parameters include:
+
 - Search terms: to specify the target information in the data sets
 - API keys: for accessing necessary authentication services
 - File paths and URLs: to specify the location of the content to be processed
@@ -19,6 +20,7 @@ Depending on the operator, the parameters include:
 ## Outputs
 
 The outputs of these operators differ based on their functionality. They include:
+
 - Data in different formats: processed and transformed according to the given inputs
 - Results of web searches, content summaries, and other forms of synthesized information
 - Interactions with online platforms, such as Github or email services
@@ -28,82 +30,109 @@ The outputs of these operators differ based on their functionality. They include
 In this section, we summarize the `run_step` function and the helper functions supporting it for each operator provided in the code:
 
 ### AskChatGpt
+
 This operator interacts with OpenAI GPT-3 for generating human-like text based on the provided input.
 
 ### WebSearch
+
 Performs a web search using an external search engine according to the given search terms.
 
+### WebsiteScraper
+
+Scrapes the contents from a given url
+
+### FileReader
+
+Reads the content from a given file, supports (.pdf, .json, and, .csv)
+
 ### GitHubFileReader
+
 Reads a file from a specified GitHub repository.
 
 ### GitHubMergeRequester
+
 Creates a merge request on GitHub with the provided information.
 
 ### GitHubDocsWriter
+
 Writes documentation content to a specified GitHub repository.
 
 ### ListProcessor
+
 Processes lists of data and performs various operations such as sorting, filtering, and other transformations.
 
-### IngestData
-Ingests data from different sources and formats.
-
 ### IndexData
+
 Indexes ingested data for efficient retrieval and processing.
 
 ### SplitData
+
 Splits data for different purposes, e.g. training, testing, and validation.
 
-### IngestPDF
-Ingests data from PDF files for further processing.
-
 ### IngestDocs
+
 Ingests data from different document formats.
 
 ### HybridSearch
+
 Performs a search combining different search strategies, such as full-text search and vector-based search.
 
 ### Summarize
+
 Summarizes pieces of content based on given criteria.
 
 ### Tweet
+
 Interacts with the Twitter platform for posting updates and retrieving information.
 
 ### ScrapeHackerNews
+
 Scrapes data from the Hacker News platform based on given conditions.
 
 ### FindBestPost
+
 Finds the best post based on user-defined criteria, such as ranking and sentiment.
 
 ### GmailSender
+
 Sends emails using the Gmail platform with the provided content.
 
 ### PersistVectorIndex
+
 Stores the vector index persistently for later retrieval.
 
 ### CastType
+
 Casts data types from one format to another.
 
 ### ChatBot
+
 Provides a chatbot interface to interact with users and handle various tasks.
 
 ### FullTextSearch
+
 Performs a full-text search on a dataset using specified terms.
 
 ### CombineStrings
+
 Combines strings based on provided rules and patterns.
 
 ### InputOperator
+
 Acts as a placeholder for providing input data to other operators.
 
 ### OutputOperator
+
 Acts as a placeholder for capturing output data from other operators.
 
 ### VectorizeOperator
+
 Converts text into a numerical vector representation.
 
 ### StoreInDb
+
 Stores data in a specified database.
 
 ### TextSearchInDb
+
 Performs a text search within a database using specified search terms.
--- operators/web_search.py
+++ operators/web_search.py
@@ -7,15 +7,16 @@ from ai_context import AiContext
 
 import json
 
+
 class WebSearch(BaseOperator):
-    @staticmethod    
+    @staticmethod
     def declare_name():
         return 'Web search'
-    
+
     @staticmethod
     def declare_category():
         return BaseOperator.OperatorCategory.CONSUME_DATA.value
-        
+
     @staticmethod
     def declare_parameters():
         return [
@@ -30,24 +31,24 @@ class WebSearch(BaseOperator):
                 "placeholder": "Enter the number of results (default 5)"
             }
         ]
-    
-    @staticmethod    
+
+    @staticmethod
     def declare_inputs():
         return []
-    
-    @staticmethod    
+
+    @staticmethod
     def declare_outputs():
         return [
             {
-                "name": "search_results",
-                # In a bit of hacky way we will store url in 'name' key and snippet in 'content'.
-                # So that to make output of this operator immedaitely consumable by ListProcessor.
-                # We should think though how detailed should the contract be between operators in terms of 
-                # type safety. 
-                "data_type": "{name,content}[]",
+                "name": "urls",
+                "data_type": "string[]",
+            },
+            {
+                "name": "snippets",
+                "data_type": "string[]"
             }
         ]
-        
+
     def run_step(self, step, ai_context):
         query = step['parameters'].get('query')
         results_count = step['parameters'].get('results_count')
@@ -55,19 +56,25 @@ class WebSearch(BaseOperator):
             results_count = 5
         else:
             results_count = int(results_count)
-        
+
         google_res = self.google_search(query, results_count, ai_context)
         snippets, urls = self.get_urls_and_snippets(google_res)
-        ai_context.add_to_log(f'Google results:\n urls={urls}\n snippets={snippets}') 
+
+        ai_context.add_to_log(
+            f'Google results:\n urls={urls}\n snippets={snippets}')
+
         ai_context.set_output(
-            'search_results', 
-            [{"name": url, "content": snippet} for url, snippet in zip(urls, snippets)],
+            'urls',
+            urls,
+            self
+        )
+        ai_context.set_output(
+            'snippets',
+            snippets,
             self
         )
 
-        
     def get_urls_and_snippets(self, google_res):
-        #data = json.loads(google_res_blob)
         data = google_res
         titles_and_snippets = []
         links = []
@@ -75,15 +82,15 @@ class WebSearch(BaseOperator):
             titles_and_snippets.append(item['title'] + " " + item['snippet'])
             links.append(item['link'])
 
+        # Snippets are brief descriptions of the website content, similar to the preview on Google Search
         return titles_and_snippets, links
 
     def google_search(self, query, num_results, ai_context) -> list[str]:
         try:
-            custom_search_engine_id = ai_context.get_secret('google_search_engine_id')
+            custom_search_engine_id = ai_context.get_secret(
+                'google_search_engine_id')
             service = build(
-                "customsearch"
-                , "v1"
-                , developerKey=ai_context.get_secret('google_search_developer_key')
+                "customsearch", "v1", developerKey=ai_context.get_secret('google_search_developer_key')
             )
             api_response = (
                 service.cse()
@@ -91,12 +98,8 @@ class WebSearch(BaseOperator):
                 .execute()
             )
 
-            # Extract the search result items from the response
+            # Extract the search result items (urls and snippets) from the response
             return api_response
-            # api_response.get("items", [])
-
-            # Create a list of only the URLs from the search results
-            #links = [item["link"] for item in results]
 
         except HttpError as e:
             invalid_api_key_str = 'invalid API key'
@@ -110,12 +113,3 @@ class WebSearch(BaseOperator):
                 return f"Error: {invalid_api_key_str}"
             else:
                 return f"Error: {e}"
-
-        return to_utf8_json_list(links)
-
-
-def to_utf8_json_list(l) -> str:
-    res = json.dumps(
-        [s.encode("utf-8", "ignore") for s in l]
-    )
-    return res
